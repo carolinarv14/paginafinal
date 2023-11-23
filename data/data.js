4dd0fa9b311d8b8e2f4699d0de818999778594f6
carolinarv14 <carolinarinconv@iesantaelena.edu.co>
2023-11-23 11:48:51 -0500

    pagina final lista

--- data/data.js
+++ data/data.js
@@ -1,14 +1,30 @@
 export const data = [
-    {
-      "id": 1,
-      "name": "arepa de queso",
-      "image": "https://achiote18.com/wp-content/uploads/2023/01/receta0008.webp",
-      "description": "Esta es una deliciosa arepa de queso."
-    },
-    {
-      "id": 2,
-      "name": "arepa tipo corazón",
-      "image": "https://img-global.cpcdn.com/recipes/7611b64dd1a88ec8/400x400cq70/photo.jpg",
-      "description": "Una arepa en forma de corazón para enamorar a tu paladar."
-    }
-  ];
+  {
+    "id": 1,
+    "name": "arepa de queso",
+    "image": "https://achiote18.com/wp-content/uploads/2023/01/receta0008.webp",
+    "description": "Esta es una deliciosa arepa de queso."
+  },
+  {
+    "id": 2,
+    "name": "arepa tipo corazón",
+    "image": "https://img-global.cpcdn.com/recipes/7611b64dd1a88ec8/400x400cq70/photo.jpg",
+    "description": "Una arepa en forma de corazón para enamorar a tu paladar."
+
+  },
+  {
+    "id": 3,
+    "name": "arepa de chocolo",
+    "image": "https://i0.wp.com/organicos.mamey.org/wp-content/uploads/2016/12/Arepas-de-mote.jpg?fit=275%2C183&ssl=1",
+    "description": "Una arepa de chocolo con queso por dentro."
+    
+  },
+  {
+    "id": 4,
+    "name": "Tipico de la cocina",
+    "image": "https://s3.amazonaws.com/rtvc-assets-senalcolombia.gov.co/s3fs-public/field/image/sancocho-plato-tipico-colombiano-ilustracion.jpg",
+    "description": "nuestro tipico acompañado de muchos tipos de carnes es el mas gustoso en nuestrio restaurante."
+    
+  },
+
+];
